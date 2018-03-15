e23c7200f039f06ba70acc119961b62772a32944
marccohen92 <marc-cohen@live.fr>
2018-03-15 19:15:25 +0100

    Package Apartment V1

---
+++ Apartments/src/Apartment/InterfaceApartment.java
@@ -0,0 +1,31 @@
+package Apartment;
+
+public interface InterfaceApartment {
+
+	double getFloorArea();
+
+	String getAdress();
+
+	int getNbChambers();
+
+	int getNbSleeping();
+
+	int getNbBR();
+
+	boolean getTerrace();
+
+	double getFloorAreaTerrace();
+
+	String getDescription();
+
+	String getTitle();
+
+	boolean getWifi();
+
+	double getPriceN();
+
+	int getNbMinNight();
+
+	boolean getTele();
+
+}
