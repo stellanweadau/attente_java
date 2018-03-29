1c8ecd017e2a25f78f1cae9caed2f124e187b702
Deeplygends <peeren.pro@gmail.com>
2018-03-29 16:41:46 +0200

    local change Micky

---
+++ src_test/piecewiseTest/PiecewiseTest.java
@@ -0,0 +1,70 @@
+package piecewiseTest;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.github.oliviercailloux.y2018.apartments.piecewise.PiecewiseLinearValueFunction;
+
+public class PiecewiseTest{
+	
+	@Test
+	void getUtilityNormalTest() {
+		PiecewiseLinearValueFunction p = initializePieceWise();
+		try
+		{
+			Assert.assertEquals(0.32, p.getUtility(36), 0);
+		}
+		catch(IOException e)
+		{
+			fail("IOException throw");
+		}
+		
+	
+	}
+	@Test
+	void getUtilityWithParamAboveMax() {
+		PiecewiseLinearValueFunction p = initializePieceWise();
+		try
+		{
+			Assert.assertEquals(1, p.getUtility(55), 0);
+		}
+		catch(IOException e)
+		{
+			fail("IOException throw");
+		}
+	}
+	
+	@Test
+	void getUtilityWithParamBelowMin() {
+		PiecewiseLinearValueFunction p = initializePieceWise();
+		try
+		{
+			Assert.assertEquals(0, p.getUtility(25), 0);
+		}
+		catch(IOException e)
+		{
+			fail("IOException throw");
+		}
+		
+	}
+	
+	@Test
+	void getLinearValueWrongArgument() {
+		PiecewiseLinearValueFunction p = initializePieceWise();
+		Assertions.assertThrows(IllegalArgumentException.class, () -> p.getLinearValue(30, 30, 0.1, 0.1));	
+	}
+	
+	private PiecewiseLinearValueFunction initializePieceWise()
+	{
+		PiecewiseLinearValueFunction p = new PiecewiseLinearValueFunction("Surface");
+		p.setUtility(30, 0.2);
+		p.setUtility(40, 0.4);
+		p.setUtility(50, 0.8);
+		return p;
+	}
+}
