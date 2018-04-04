7bc610ff60fb9d7a1b5e99d37dde45d6199bf3cf
DAYAUX <guillaume.dayaux@gmail.com>
2018-04-04 23:23:19 +0200

    Commit Resource2 + Modifs File1

---
+++ src_test/readApartmentsTest/ReadApartmentsXMLFormatTest.java
@@ -0,0 +1,14 @@
+package readApartmentsTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReadApartmentsXMLFormatTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
