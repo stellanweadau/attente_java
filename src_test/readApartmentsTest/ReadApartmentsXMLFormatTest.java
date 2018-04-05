4c5543bc44c559db06328d107feb7a93ba0df7ec
DAYAUX <guillaume.dayaux@gmail.com>
2018-04-05 20:43:29 +0200

    Commit Marc Cohen

--- src_test/readApartmentsTest/ReadApartmentsXMLFormatTest.java
+++ src_test/readApartmentsTest/ReadApartmentsXMLFormatTest.java
@@ -1,14 +1,49 @@
 package readApartmentsTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+
+import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
+import io.github.oliviercailloux.y2018.apartments.readapartments.ReadApartmentsXMLFormat;
+
 
 class ReadApartmentsXMLFormatTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void readApartmentTest() throws IOException{
+		
+		ReadApartmentsXMLFormat r = initializeReadApartmentsXMLFormat();
+		
+		File f = new File("resources\\start-apartment-classpath.xml");
+		
+		try (InputStream input = new FileInputStream(f)){
+
+			Apartment a = r.readApartment(input);
+			
+			String s = "5 avenue Roger Salengro 92370 Chaville France ";
+			
+			Assert.assertEquals(s,a.getAddress(),"Address doesn't match with the address in the XML file");
+
+		}
+
+		
+	}
+	
+	private ReadApartmentsXMLFormat initializeReadApartmentsXMLFormat() {
+		
+		ReadApartmentsXMLFormat r = new ReadApartmentsXMLFormat();
+		
+		return r;
 	}
 
 }
