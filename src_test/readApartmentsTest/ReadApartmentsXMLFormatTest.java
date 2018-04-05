c185f22d230d4b64456392924583ebfc85c4c555
DAYAUX <guillaume.dayaux@gmail.com>
2018-04-05 21:04:19 +0200

    Commit Marc Cohen 2

--- src_test/readApartmentsTest/ReadApartmentsXMLFormatTest.java
+++ src_test/readApartmentsTest/ReadApartmentsXMLFormatTest.java
@@ -20,23 +20,92 @@ import io.github.oliviercailloux.y2018.apartments.readapartments.ReadApartmentsX
 class ReadApartmentsXMLFormatTest {
 
 	@Test
-	void readApartmentTest() throws IOException{
-		
+	void readApartmentAddressTest() throws IOException{
+
 		ReadApartmentsXMLFormat r = initializeReadApartmentsXMLFormat();
-		
+
 		File f = new File("resources\\start-apartment-classpath.xml");
-		
+
 		try (InputStream input = new FileInputStream(f)){
 
 			Apartment a = r.readApartment(input);
-			
-			String s = "5 avenue Roger Salengro 92370 Chaville France ";
-			
+
+			String s = "5 avenue Roger Salengro 92370 Chaville France";
+
 			Assert.assertEquals(s,a.getAddress(),"Address doesn't match with the address in the XML file");
+		}
+	}
+		
+	@Test
+	void readApartmentFloorAreaTest() throws IOException{
+
+		ReadApartmentsXMLFormat r = initializeReadApartmentsXMLFormat();
+
+		File f = new File("resources\\start-apartment-classpath.xml");
+
+		try (InputStream input = new FileInputStream(f)){
+
+			Apartment a = r.readApartment(input);
+
+			Assert.assertEquals(a.getFloorArea(),150.0,0);
 
 		}
 
-		
+
+	}
+	
+	@Test
+	void readApartmentNbSleepingTest() throws IOException{
+
+		ReadApartmentsXMLFormat r = initializeReadApartmentsXMLFormat();
+
+		File f = new File("resources\\start-apartment-classpath.xml");
+
+		try (InputStream input = new FileInputStream(f)){
+
+			Apartment a = r.readApartment(input);
+
+			Assert.assertEquals(a.getNbSleeping(),5,0);
+
+		}
+
+
+	}
+	
+	@Test
+	void readApartmentFloorAreaTerraceTest() throws IOException{
+
+		ReadApartmentsXMLFormat r = initializeReadApartmentsXMLFormat();
+
+		File f = new File("resources\\start-apartment-classpath.xml");
+
+		try (InputStream input = new FileInputStream(f)){
+
+			Apartment a = r.readApartment(input);
+
+			Assert.assertEquals(a.getFloorAreaTerrace(),160,0);
+
+		}
+
+
+	}
+	
+	@Test
+	void readApartmentPricePerNightTest() throws IOException{
+
+		ReadApartmentsXMLFormat r = initializeReadApartmentsXMLFormat();
+
+		File f = new File("resources\\start-apartment-classpath.xml");
+
+		try (InputStream input = new FileInputStream(f)){
+
+			Apartment a = r.readApartment(input);
+
+			Assert.assertEquals(a.getPricePerNight(),3,0);
+
+		}
+
+
 	}
 	
 	private ReadApartmentsXMLFormat initializeReadApartmentsXMLFormat() {
