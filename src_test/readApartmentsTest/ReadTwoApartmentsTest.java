0aed780837f89504236194fdf6705d464ff93592
DAYAUX <guillaume.dayaux@gmail.com>
2018-04-10 11:02:14 +0200

    Modif classpath

--- src_test/readApartmentsTest/ReadTwoApartmentsTest.java
+++ src_test/readApartmentsTest/ReadTwoApartmentsTest.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+
 import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
 import io.github.oliviercailloux.y2018.apartments.readapartments.ReadApartmentsXMLFormat;
 
@@ -27,11 +29,13 @@ public class ReadTwoApartmentsTest {
 		ReadApartmentsXMLFormat r = new ReadApartmentsXMLFormat();
 		ReadApartmentsXMLFormat r1 = new ReadApartmentsXMLFormat();
 
-		File f = new File("resources\\start-apartment-classpath.xml");
+		//File f = new File("resources\\start-apartment-classpath.xml"); 
+		
+		
 		String f1 = new File("start-apartment.xml").getAbsolutePath();
 
-		try (InputStream input = new FileInputStream(f)){
-			Apartment a = r.readApartment(input);
+		try (InputStream f = ReadTwoApartmentsTest.class.getResourceAsStream("start-apartment-classpath.xml")){
+			Apartment a = r.readApartment(f);
 			System.out.println(a);
 		}
 
