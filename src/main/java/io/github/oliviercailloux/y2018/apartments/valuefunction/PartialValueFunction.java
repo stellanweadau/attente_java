7f854bb796c79113c89ac11b56950d65917b5f60
marccohen92 <marc-cohen@live.fr>
2018-05-05 11:05:14 +0200

    Interface PartialValueFonction

---
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/PartialValueFunction.java
@@ -0,0 +1,11 @@
+package io.github.oliviercailloux.y2018.apartments.valuefunction;
+
+import java.util.function.Function;
+
+
+public interface PartialValueFunction<T> extends Function<T, Double> {
+	
+	@Override
+	public String toString();
+
+}
