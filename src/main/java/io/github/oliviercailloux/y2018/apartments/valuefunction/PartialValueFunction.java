1cd966a9865211dbfaa7113ca03c1e042c4a07f5
SlowVirtuousGiant <nicolas.xu1997@gmail.com>
2018-05-12 12:11:56 +0200

    retour en arriÃere

--- src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/PartialValueFunction.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/PartialValueFunction.java
@@ -1,5 +1,7 @@
 package io.github.oliviercailloux.y2018.apartments.valuefunction;
 
+import java.util.function.Function;
+
 public interface PartialValueFunction<T> extends Function<T, Double> {
 	
 	@Override
