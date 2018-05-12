770ad73e3c4d8b129c859c011d65381bcd538e1c
SlowVirtuousGiant <nicolas.xu1997@gmail.com>
2018-05-12 11:48:01 +0200

    Changement pom

--- src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/PartialValueFunction.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/PartialValueFunction.java
@@ -1,8 +1,5 @@
 package io.github.oliviercailloux.y2018.apartments.valuefunction;
 
-import java.util.function.Function;
-
-
 public interface PartialValueFunction<T> extends Function<T, Double> {
 	
 	@Override
