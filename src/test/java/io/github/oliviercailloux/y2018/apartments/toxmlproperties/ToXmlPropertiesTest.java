6a9aa2fceb8bfc1bbf17f08eadd996d1342a0a38
Deeplygends <peeren.pro@gmail.com>
2018-05-14 21:07:26 +0200

    XmlPropertiesTest

--- src/test/java/io/github/oliviercailloux/y2018/apartments/toxmlproperties/ToXmlPropertiesTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/toxmlproperties/ToXmlPropertiesTest.java
@@ -15,6 +15,15 @@ import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
 import io.github.oliviercailloux.y2018.apartments.readapartments.ReadApartmentsXMLFormat;
 
 class ToXmlPropertiesTest {
+	@Test
 
-
+	void readApartmentTest() throws IllegalArgumentException, IllegalAccessException, IOException
+	{
+		XMLProperties j = new XMLProperties();
+		Apartment a = new Apartment(80.5, "6 rue des paquerette 74000 Annecy", "Petit Manoir de campagne");
+		File f = new File("src/test/resources/xmlfile.xml");
+		j.toXML(a, f);
+		
+		// TO DO test que ça s'est bien écris dans le fichier xml
+	}
 }
