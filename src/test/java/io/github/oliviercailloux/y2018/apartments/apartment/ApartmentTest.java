4403d230bae84c204602702f795427eba0b0cf02
DAYAUX <guillaume.dayaux@gmail.com>
2018-05-15 11:45:43 +0200

    Ajout Test equals Apartment

---
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/apartment/ApartmentTest.java
@@ -0,0 +1,29 @@
+package io.github.oliviercailloux.y2018.apartments.apartment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApartmentTest {
+
+	@Test
+	void equalsTestTrue() {
+		
+		Apartment a1 = new Apartment(150,"Arras","Micky's home");
+		Apartment a2 = new Apartment(150,"Arras","Micky's home");
+		
+		assertTrue(a1.equals(a2));
+		
+	}
+	
+	@Test
+	void equalsTestFalse() {
+		
+		Apartment a1 = new Apartment(150,"Chaville","Marc's home");
+		Apartment a2 = new Apartment(150,"Arras","Micky's home");
+		
+		assertFalse(a1.equals(a2));
+		
+	}
+
+}
