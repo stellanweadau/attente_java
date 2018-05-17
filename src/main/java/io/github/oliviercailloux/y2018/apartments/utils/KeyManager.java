0c0a6a47e506132e3479542157fa534d714f5da9
DAYAUX <guillaume.dayaux@gmail.com>
2018-05-17 17:00:59 +0200

    Modif test DistanceSubway 2

--- src/main/java/io/github/oliviercailloux/y2018/apartments/utils/KeyManager.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/utils/KeyManager.java
@@ -1,53 +1,59 @@
 package io.github.oliviercailloux.y2018.apartments.utils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class KeyManager {
-	
-	public static String getApiKey() {
-		String apiKey="";
-		String fichier = new File("API_KEY.txt").getAbsolutePath();
-		
-		try(FileReader fr = new FileReader (fichier)){
-			int c = fr.read();
-			
 
-			while (c != -1)
-			{
-				apiKey+=(char) c;
-				c = fr.read();
-			}
-		}
-		catch (IOException exception)
-		{
-		    System.out.println ("Reading error : "+ exception.getMessage());
-		}
+	public static String getApiKey() throws FileNotFoundException, IOException {
+
+		String apiKey = System.getenv("APIKEY");
 		
+
+		if (apiKey == null) {
+			String fichier = new File("API_KEY.txt").getAbsolutePath();
+			apiKey = "";
+
+			try(FileReader fr = new FileReader (fichier)){
+				int c = fr.read();
+
+
+				while (c != -1)
+				{
+					apiKey+=(char) c;
+					c = fr.read();
+				}
+			}
+
+		}
+
 		return apiKey;
 	}
-	
-	public static String getGeocodeApiKey() {
-		
-		String geocodeApiKey="";
-		String fichier = new File("GEOCODE_API_KEY.txt").getAbsolutePath();
-		
-		try(FileReader fr = new FileReader (fichier)){
-			int c = fr.read();
-			
 
-			while (c != -1)
-			{
-				geocodeApiKey+=(char) c;
-				c = fr.read();
+	public static String getGeocodeApiKey() throws FileNotFoundException, IOException {
+
+		String geocodeApiKey = System.getenv("GEOCODEAPIKEY");
+
+		if (geocodeApiKey == null) {
+			String fichier = new File("GEOCODE_API_KEY.txt").getAbsolutePath();
+
+			geocodeApiKey="";
+
+			try(FileReader fr = new FileReader (fichier)){
+				int c = fr.read();
+
+
+				while (c != -1)
+				{
+					geocodeApiKey+=(char) c;
+					c = fr.read();
+				}
 			}
+
 		}
-		catch (IOException exception)
-		{
-		    System.out.println ("Reading error : "+ exception.getMessage());
-		}
-		
+
 		return geocodeApiKey;
 	}
 
