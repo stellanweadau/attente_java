543ee8167be424def000946bbfa4336621ef0f96
SlowVirtuousGiant <nicolas.xu1997@gmail.com>
2018-06-02 18:50:05 +0200

    do a barrel roll

--- src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/ApartmentValueFunctionTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/ApartmentValueFunctionTest.java
@@ -17,31 +17,55 @@ class ApartmentValueFunctionTest {
 
 	@Test
 	void apartmentValueFunctionTest() throws NumberFormatException, InvalidPropertiesFormatException, IOException{
-		ReadApartmentsXMLFormat r = new ReadApartmentsXMLFormat();
+		Apartment a = new Apartment(250, "108 rue de chat-ville Ville-d'Avray 92410", "Une ferme");
+		a.setNbSleeping(4);
+		a.setNbMinNight(3);
+		a.setTerrace(true);
+		a.setNbBedrooms(1);
+		a.setPricePerNight(3.3);
+		a.setTele(false);
+		a.setWifi(false);
+		a.setNbBathrooms(1);
+		a.setDescription("Une ferme rustique en compagnie de Dwight Schrute, interdit à Jim Halpert");
+		a.setFloorAreaTerrace(40);
 
-		try(InputStream f = ReadApartmentsXMLFormat.class.getResourceAsStream("xmlfileTest.xml")){
-			Apartment a = r.readApartment(f);
+		ApartmentValueFunction valueFunction = new ApartmentValueFunction();
 
-			ApartmentValueFunction valueFunction = new ApartmentValueFunction();
+		LinearValueFunction floorAreaV = new LinearValueFunction(0,200.00);
+		valueFunction.setFloorAreaValueFunction(floorAreaV);
+		
+		LinearValueFunction nbSleepingV = new LinearValueFunction(3,5);
+		valueFunction.setNbSleepingValueFunction(nbSleepingV);
 
+		ReversedLinearValueFunction nbMinNightV = new ReversedLinearValueFunction(7,30);
+		valueFunction.setNbMinNightValueFunction(nbMinNightV);
 
-			LinearValueFunction floorAreaV = new LinearValueFunction(0,200.00);
-			valueFunction.setFloorAreaValueFunction(floorAreaV);
+		BooleanValueFunction terraceV = new BooleanValueFunction(true);
+		valueFunction.setTerraceValueFunction(terraceV);
 
-			BooleanValueFunction wifiV = new BooleanValueFunction(true);
-			valueFunction.setWifiValueFunction(wifiV);
+		LinearValueFunction nbBedroomsV = new LinearValueFunction(3,4);
+		valueFunction.setNbBedroomsValueFunction(nbBedroomsV);
 
-			BooleanValueFunction teleV = new BooleanValueFunction(true);
-			valueFunction.setTeleValueFunction(teleV); 
+		ReversedLinearValueFunction pricePerNightV = new ReversedLinearValueFunction(20,40);
+		valueFunction.setPricePerNightValueFunction(pricePerNightV);
 
+		BooleanValueFunction teleV = new BooleanValueFunction(true);
+		valueFunction.setTeleValueFunction(teleV); 
 
-			Assert.assertEquals(0.1, valueFunction.getSubjectiveValue(a),0.0001);
+		BooleanValueFunction wifiV = new BooleanValueFunction(true);
+		valueFunction.setWifiValueFunction(wifiV);
+		
+		LinearValueFunction nbBathroomsV = new LinearValueFunction(2,3);
+		valueFunction.setNbBathroomsValueFunction(nbBathroomsV);
+		
+		LinearValueFunction floorAreaTerraceV = new LinearValueFunction(30,50);
+		valueFunction.setFloorAreaTerraceValueFunction(floorAreaTerraceV);
 
-			valueFunction.setTeleSubjectiveValueWeight(10.0);
-
-			Assert.assertEquals(0.0091743, valueFunction.getSubjectiveValue(a),0.00001);
-
-		}
+		Assert.assertEquals(0.5, valueFunction.getSubjectiveValue(a),0.0001);
+		
+		valueFunction.setTeleSubjectiveValueWeight(10);
+		
+		Assert.assertEquals(0.04587, valueFunction.getSubjectiveValue(a),0.00001);
 
 	}
 
