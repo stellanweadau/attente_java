8380cba132414c64a91687464801cfa48c9a64f2
Amioplk <amir.worms@dauphine.eu>
2019-05-19 17:38:03 +0200

    Begin XMCDA

---
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/xmcda/XMCDAHandeler.java
@@ -0,0 +1,13 @@
+package io.github.oliviercailloux.y2018.apartments.xmcda;
+
+import org.decisiondeck.jmcda.exc.InvalidInputException;
+import org.decisiondeck.jmcda.xws.IXWS;
+
+public class XMCDAHandeler implements IXWS {
+
+	@Override
+	public void execute() throws InvalidInputException {
+		
+	}
+
+}
