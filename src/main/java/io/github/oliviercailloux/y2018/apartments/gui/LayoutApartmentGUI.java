30e0bc63903d1361c856252736c4ed083397d69e
zeinaalwazzan <zeinawazzan@live.com>
2019-06-20 18:54:36 +0200

    Mise à jour taille shell

--- src/main/java/io/github/oliviercailloux/y2018/apartments/gui/LayoutApartmentGUI.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/gui/LayoutApartmentGUI.java
@@ -2,15 +2,13 @@ package io.github.oliviercailloux.y2018.apartments.gui;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-
 import org.eclipse.swt.*;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.DOMException;
-
-import com.google.common.collect.Ordering;
 
 import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
 import io.github.oliviercailloux.y2018.apartments.toxmlproperties.XMLProperties;
@@ -19,55 +17,86 @@ import io.github.oliviercailloux.y2018.apartments.valuefunction.LinearValueFunct
 
 import org.eclipse.swt.layout.*;
 
+/**
+ * @author AlWAZZAN & SAKHO
+ * 
+ */
+
+/**
+ * this class displays a list of apartments sorted according to the user's
+ * utilities
+ */
+
 public class LayoutApartmentGUI {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(CreateApartmentGUI.class);
 
 	static Display display;
 	static Shell shell;
 
-	public static void affiche(ApartmentValueFunction avf) throws DOMException, IllegalAccessException, IOException {
+	/**
+	 * This is the main function
+	 * 
+	 * @param args
+	 * @throws IllegalAccessException
+	 * @throws DOMException
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws DOMException, IllegalAccessException, IOException {
+		ApartmentValueFunction avf = new ApartmentValueFunction();
+		avf.setFloorAreaValueFunction(new LinearValueFunction(0, 300));
+
+		ArrayList<Apartment> listap = getListSorted(avf);
+		displayAppart(listap);
+	}
+
+	/**
+	 * General method which displays all the sorted apartment
+	 * 
+	 * @param listApp the list of apartments to display
+	 * @throws DOMException
+	 * @throws IllegalAccessException
+	 * @throws IOException
+	 */
+
+	public static void displayAppart(ArrayList<Apartment> listApp) {
 		Label adresse;
 		Label surface;
 		Label prix;
 		Label nbrChambres;
 		Canvas photoCanevas;
 
-// definition d'un display
+		// define a display
 		display = new Display();
 		shell = new Shell(display);
 		shell.setText("Sélection d'un appartement");
 
+		// create a gridLayout of 2 columns
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 3;
 		shell.setLayout(gridLayout);
 
+		// create a griddata for a list of objects
 		GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
 		gridData.horizontalSpan = 2;
 
+		// create a list label
 		Label label = new Label(shell, SWT.NULL);
 		label.setText("Liste des appartements disponibles :");
 		label.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
-
 		new Label(shell, SWT.NULL);
 
+		// define griddata with a verticalspan : 3 rows
 		new Label(shell, SWT.NULL);
-
 		gridData = new GridData(GridData.FILL_BOTH);
 		gridData.verticalSpan = 3;
 
-		ArrayList<Apartment> appart = new ArrayList<Apartment>(10);
-		for (int i = 0; i < 50; ++i) {
-			Apartment a = XMLProperties.generateRandomXML();
-			appart.add(a);
-		}
-		appart.sort(
-				(Apartment c, Apartment d) -> (int) ((avf.getSubjectiveValue(c) - avf.getSubjectiveValue(d)) * 100000));
-
+		// add objects of apartments in a list
 		final List list = new List(shell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
 
-		for (Apartment a : appart) {
+		for (Apartment a : listApp) {
 			System.out.println("Appart : " + a);
 			list.add("Titre: " + a.getTitle() + "\t" + " Adresse : " + a.getAddress());
-			System.out.println(avf.getSubjectiveValue(a));
 		}
 
 		gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_FILL);
@@ -76,10 +105,7 @@ public class LayoutApartmentGUI {
 		list.setLayoutData(gridData);
 
 		Group appartInfo = new Group(shell, SWT.NULL);
-		appartInfo.setText("Détail dur l'appartement sélectionné :");
-// on refait un grisd coupé eb 2 pour aligner label et contenu.
-// on pourrai peut etre mettre en read - a tester pour voir si ca se rafraichi
-// quand meme
+		appartInfo.setText("Détail sur l'appartement sélectionné :");
 
 		gridLayout = new GridLayout();
 		gridLayout.numColumns = 2;
@@ -89,6 +115,7 @@ public class LayoutApartmentGUI {
 		gridData.horizontalSpan = 2;
 		appartInfo.setLayoutData(gridData);
 
+		// beginning of creation of labels
 		new Label(shell, SWT.NULL).setText("Photo : ");
 		photoCanevas = new Canvas(shell, SWT.BORDER);
 
@@ -111,45 +138,60 @@ public class LayoutApartmentGUI {
 		gridData = new GridData(GridData.HORIZONTAL_ALIGN_END);
 		gridData.horizontalSpan = 3;
 
+		// on click on an apartment
 		list.addSelectionListener(new SelectionAdapter() {
 
 			@Override
 			public void widgetSelected(SelectionEvent event) {
 				int[] selectedItems = list.getSelectionIndices();
-// String outString = "";
+
 				for (int loopIndex = 0; loopIndex < selectedItems.length; loopIndex++) {
-					adresse.setText(appart.get(list.getSelectionIndex()).getAddress());
-					surface.setText(" " + appart.get(list.getSelectionIndex()).getFloorArea());
-					prix.setText(" " + appart.get(list.getSelectionIndex()).getPricePerNight());
-					nbrChambres.setText(" " + appart.get(list.getSelectionIndex()).getNbBedrooms());
+					adresse.setText(listApp.get(list.getSelectionIndex()).getAddress());
+					surface.setText(" " + listApp.get(list.getSelectionIndex()).getFloorArea());
+					prix.setText(" " + listApp.get(list.getSelectionIndex()).getPricePerNight());
+					nbrChambres.setText(" " + listApp.get(list.getSelectionIndex()).getNbBedrooms());
 				}
-
 			}
-
 		});
 
-		shell.setSize(1500, 550);
-
+		shell.setSize(1000, 550);
 		shell.open();
+		LOGGER.info("The Shell was opened with success.");
 
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
+		LOGGER.info("The screen was closed with success.");
 
-// desallocation manuelle ( fichier gourmand - suppression au cas où garbadge
-// collector ne desaloue pas )
+		// photos to be added lately
 		if (photoCanevas != null) {
 			photoCanevas.dispose();
 		}
 	}
 
-	public static void main(String[] args) throws DOMException, IllegalAccessException, IOException {
+	/**
+	 * Method that creates an apartment list randomly and sort it
+	 * 
+	 * @param avf a way to rate the apartments
+	 * @throws DOMException
+	 * @throws IllegalAccessException
+	 * @throws IOException
+	 */
 
-		ApartmentValueFunction avf = new ApartmentValueFunction();
-		avf.setFloorAreaValueFunction(new LinearValueFunction(0, 300));
+	public static ArrayList<Apartment> getListSorted(ApartmentValueFunction avf)
+			throws IOException, IllegalAccessException {
+		ArrayList<Apartment> appart = new ArrayList<Apartment>();
+		for (int i = 0; i < 50; ++i) {
+			Apartment a = XMLProperties.generateRandomXML(); // lately we will use generateRandom
+			appart.add(a);
+		}
 
-		affiche(avf);
+		// trier la liste selon l'utilité
+		appart.sort(
+				(Apartment c, Apartment d) -> (int) ((avf.getSubjectiveValue(c) - avf.getSubjectiveValue(d)) * 100000));
+
+		return appart;
 
 	}
 
