7d617cceaa92dd191c899b73397ac070a3ac157e
Amioplk <amir.worms@dauphine.eu>
2019-06-21 15:29:37 +0200

    gitignore again

--- src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/ConstantValueFunction.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/ConstantValueFunction.java
@@ -8,6 +8,16 @@ package io.github.oliviercailloux.y2018.apartments.valuefunction;
 
 public class ConstantValueFunction<T> implements PartialValueFunction<T> {
 
+	double subjectiveValue;
+	
+	ConstantValueFunction(){
+		this(0d);
+	}
+	
+	public ConstantValueFunction(double d) {
+		this.subjectiveValue = d;
+	}
+
 	@Override
 	public Double apply(T objectiveData) {
 		return getSubjectiveValue(objectiveData);
@@ -15,8 +25,7 @@ public class ConstantValueFunction<T> implements PartialValueFunction<T> {
 
 	@Override
 	public double getSubjectiveValue(T objectiveData) throws IllegalArgumentException {
-		return 0;
+		return this.subjectiveValue;
 	}
-
 
 }
