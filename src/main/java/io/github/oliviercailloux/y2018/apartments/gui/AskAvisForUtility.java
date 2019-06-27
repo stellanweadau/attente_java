571c4c898f72831b0204a3d83a97824afb0362f9
Sandra Salame <sandrafsalame@gmail.com>
2019-06-27 13:56:23 +0200

    ajout fonction interface

--- src/main/java/io/github/oliviercailloux/y2018/apartments/gui/AskAvisForUtility.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/gui/AskAvisForUtility.java
@@ -1,22 +1,10 @@
 package io.github.oliviercailloux.y2018.apartments.gui;
 
 
-import java.io.IOException;
-import java.util.ArrayList;
-import org.eclipse.swt.*;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWT.*;
 import org.eclipse.swt.widgets.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.w3c.dom.DOMException;
 
-import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
-import io.github.oliviercailloux.y2018.apartments.toxmlproperties.XMLProperties;
-import io.github.oliviercailloux.y2018.apartments.valuefunction.ApartmentValueFunction;
-import io.github.oliviercailloux.y2018.apartments.valuefunction.LinearValueFunction;
-
-import org.eclipse.swt.layout.*;
 
 /**
  * @author SALAME & SAKHO
@@ -30,10 +18,25 @@ import org.eclipse.swt.layout.*;
 
 public class AskAvisForUtility {
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(CreateApartmentGUI.class);
-
-	static Display display;
-	static Shell shell;
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		Label label = new Label(shell, SWT.CENTER);
+		label.setText("Bonjour!");
+		label.pack();
+		shell.pack();
+		shell.open();
+		
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()); 
+			display.sleep();
+			display.dispose();
+			label.dispose();
+		}
+	}
+	
+	
+	
 	
 	
 
