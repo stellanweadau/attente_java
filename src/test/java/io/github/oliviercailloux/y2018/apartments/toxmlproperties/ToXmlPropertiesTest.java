586f69ee07e03330fd7828229441c01d10af6808
zeinaalwazzan <zeinawazzan@live.com>
2019-06-27 16:45:51 +0200

    ToXmlPropertiesTest.java is updated with junit5

--- src/test/java/io/github/oliviercailloux/y2018/apartments/toxmlproperties/ToXmlPropertiesTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/toxmlproperties/ToXmlPropertiesTest.java
@@ -5,7 +5,11 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import org.junit.Assert;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
 import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
@@ -42,19 +46,19 @@ class ToXmlPropertiesTest {
 
 			Apartment a1 = r.readApartment(f1);
 
-			Assert.assertEquals("Address doesn't match with the address in the XML file","118 rue du père noel 77480",a1.getAddress());
-			Assert.assertEquals("Title doesn't match with the title in the XML file","Grand Igloo",a1.getTitle());
-			Assert.assertEquals("Description doesn't match with the description in the XML file","Un igloo tout mignon en compagnie du père noël et de la mère noël",a1.getDescription());
-			Assert.assertEquals("Number of sleepings doesn't match with the number of sleepings in the XML file",10,a1.getNbSleeping());
-			Assert.assertEquals("Number of bathrooms doesn't match with the number of bathrooms in the XML file",1,a1.getNbBathrooms());
-			Assert.assertEquals("Number of bedrooms doesn't match with the number of bedrooms in the XML file",5,a1.getNbBedrooms());
-			Assert.assertEquals("Floor area doesn't match with the floor area in the XML file",1182118.48,a1.getFloorArea(),0);
-			Assert.assertEquals("Floor area terrace doesn't match with the floor area terrace in the XML file",8.6,a1.getFloorAreaTerrace(),0);
-			Assert.assertEquals("Price per night doesn't match with the price per night in the XML file",404.0,a1.getPricePerNight(),0);
-			Assert.assertEquals("Minimum number of nights doesn't match with the minimum number of nights in the XML file",1,a1.getNbMinNight());
-			Assert.assertFalse("The value of boolean tele doesn't match with the value of tele in the XML File",a1.getTele());
-			Assert.assertTrue("The value of boolean terrace doesn't match with the value of terrace in the XML File",a1.getTerrace());
-			Assert.assertTrue("The value of boolean wifi doesn't match with the value of wifi in the XML File",a1.getWifi());
+			assertEquals("118 rue du père noel 77480",a1.getAddress());
+			assertEquals("Grand Igloo",a1.getTitle());
+			assertEquals("Un igloo tout mignon en compagnie du père noël et de la mère noël",a1.getDescription());
+			assertEquals(10,a1.getNbSleeping());
+			assertEquals(1,a1.getNbBathrooms());
+			assertEquals(5,a1.getNbBedrooms());
+			assertEquals(1182118.48,a1.getFloorArea());
+			assertEquals(8.6,a1.getFloorAreaTerrace());
+			assertEquals(404.0,a1.getPricePerNight());
+			assertEquals(1,a1.getNbMinNight());
+			assertFalse(a1.getTele());
+			assertTrue(a1.getTerrace());
+			assertTrue(a1.getWifi());
 			
 		}
 	
