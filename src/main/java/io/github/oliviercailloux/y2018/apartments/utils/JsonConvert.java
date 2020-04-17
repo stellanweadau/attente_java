cba18d49eac942bb840d10972629568a2de3e949
MorganeFt <morganemarielea@hotmail.fr>
2020-04-17 09:58:22 +0200

    Formating code + comments in the POM

--- src/main/java/io/github/oliviercailloux/y2018/apartments/utils/JsonConvert.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/utils/JsonConvert.java
@@ -20,20 +20,24 @@ import java.util.ArrayList;
 
 import javax.json.bind.Jsonb;
 
-
 /**
- * The Class JsonConvert contains all function to transform Apartment object to JSON and vice-versa.
+ * The Class JsonConvert contains all function to transform Apartment object to
+ * JSON and vice-versa.
+ * 
  * @author Etienne CARTIER & Morgane FIOT
  */
 public abstract class JsonConvert {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(JsonConvert.class);
-	
-	/** The Constant APARTMENT_PATH_JSON gives the default location for JSON file. */
-	private static final String APARTMENT_PATH_JSON = "Apartment_Json.json";
-	
+
 	/**
-	 * Converts an Apartment object to a JSON file with the default path APARTMENT_PATH_JSON.
+	 * The Constant APARTMENT_PATH_JSON gives the default location for JSON file.
+	 */
+	private static final String APARTMENT_PATH_JSON = "Apartment_Json.json";
+
+	/**
+	 * Converts an Apartment object to a JSON file with the default path
+	 * APARTMENT_PATH_JSON.
 	 *
 	 * @param a <i>Apartment</i> object to convert into JSON
 	 * @throws IOException if the JSON file can't be created.
@@ -41,29 +45,28 @@ public abstract class JsonConvert {
 	public static void apartmentToJson(Apartment a) throws IOException {
 		apartmentToJson(a, APARTMENT_PATH_JSON);
 	}
-	
+
 	/**
 	 * Converts an Apartment object to a JSON file.
 	 *
-	 * @param a <i>Apartment</i> object to convert into JSON
+	 * @param a        <i>Apartment</i> object to convert into JSON
 	 * @param jsonPath <i>String</i> the path where to create the JSON file
 	 * @throws IOException if the JSON file can't be created.
 	 */
 	public static void apartmentToJson(Apartment a, String jsonPath) throws IOException {
-		
+
 		File jsonFile = new File(jsonPath);
 		Jsonb jsonb = JsonbBuilder.create();
-		
-		
+
 		try (Writer jsonFilePath = new BufferedWriter(new FileWriter(jsonFile))) {
 			jsonFilePath.write(jsonb.toJson(a));
 			jsonFilePath.close();
-			
+
 			LOGGER.info("Apartment has been converted with success");
-		}		
-		
+		}
+
 	}
-	
+
 	/**
 	 * Read a JSON file which contains an Apartment.
 	 *
@@ -72,14 +75,14 @@ public abstract class JsonConvert {
 	 * @throws IOException if the file can't be convert into JSON format.
 	 */
 	public static String readApartmentFromJson(String jsonPath) throws IOException {
-		
+
 		File jsonFile = new File(jsonPath);
-		
+
 		try (BufferedReader jsonFilePath = new BufferedReader(new FileReader(jsonFile))) {
-			
+
 			String jsonLine = jsonFilePath.readLine();
 			String jsonRead = jsonLine;
-			
+
 			while (true) {
 				jsonLine = jsonFilePath.readLine();
 				if (jsonLine == null) {
@@ -87,14 +90,14 @@ public abstract class JsonConvert {
 				}
 				jsonRead = jsonRead + "\n" + jsonLine;
 			}
-			
+
 			jsonFilePath.close();
 			LOGGER.info("Apartment has been read with success");
-		
+
 			return jsonRead;
-		}	
+		}
 	}
-	
+
 	/**
 	 * Gets the address field from an Address JSON.
 	 *
@@ -102,60 +105,64 @@ public abstract class JsonConvert {
 	 * @return <i>String</i> the address field
 	 */
 	public static String getAddressFromJson(String jsonString) {
-        Jsonb jsonb = JsonbBuilder.create();
+		Jsonb jsonb = JsonbBuilder.create();
 
-        final LinkedHashMap<?, ?> result = jsonb.fromJson(jsonString, LinkedHashMap.class);
-        LOGGER.info("Get address");
-        String address = result.get("address").toString();
-        
-        if (address.isEmpty()) {
+		final LinkedHashMap<?, ?> result = jsonb.fromJson(jsonString, LinkedHashMap.class);
+		LOGGER.info("Get address");
+		String address = result.get("address").toString();
+
+		if (address.isEmpty()) {
 			throw new IllegalArgumentException();
 		} else {
 			return result.get("address").toString();
 		}
-    }
-	
+	}
+
 	/**
 	 * Converts a JSON expression to an Apartment object.
 	 *
-	 * @param jsonString <i>String</i> the JSON expression to convert into Apartment object
+	 * @param jsonString <i>String</i> the JSON expression to convert into Apartment
+	 *                   object
 	 * @return <i>Apartment</i> the Apartment generated
 	 */
 	public static Apartment jsonToApartment(String jsonString) {
 		Jsonb jsonb = JsonbBuilder.create();
 		LOGGER.info("Create Json builder");
-		
+
 		Apartment.Builder apartBuild = jsonb.fromJson(jsonString, Apartment.Builder.class);
 		LOGGER.info("Apartment created from JSON");
-		
+
 		return apartBuild.build();
 	}
 
 	/**
 	 * Converts a JSON expression to a list of Apartments.
 	 *
-	 * @param jsonString <i>String</i> the JSON expression to convert into a list of Apartments
+	 * @param jsonString <i>String</i> the JSON expression to convert into a list of
+	 *                   Apartments
 	 * @return <i>ArrayList</i> the list of Apartments created
 	 */
 	public static List<Apartment> jsonToApartments(String jsonString) {
 		List<Apartment.Builder> apartmentsBuild;
 		List<Apartment> apartments = new ArrayList<Apartment>();
 		LOGGER.info("Create ArrayList of Apartment");
-		
+
 		Jsonb jsonb = JsonbBuilder.create();
 		LOGGER.info("Create Json builder");
-		
-		apartmentsBuild = jsonb.fromJson(jsonString, new ArrayList<Apartment.Builder>(){}.getClass().getGenericSuperclass());
-		
+
+		apartmentsBuild = jsonb.fromJson(jsonString, new ArrayList<Apartment.Builder>() {
+		}.getClass().getGenericSuperclass());
+
 		for (int i = 0; i < apartmentsBuild.size(); i++) {
 			apartments.add(apartmentsBuild.get(i).build());
 		}
-		
+
 		return apartments;
 	}
-	
+
 	/**
-	 * Converts a list of Apartments to a JSON file with the default path APARTMENT_PATH_JSON.
+	 * Converts a list of Apartments to a JSON file with the default path
+	 * APARTMENT_PATH_JSON.
 	 *
 	 * @param a <i>ArrayList</i> object to convert into JSON
 	 * @throws IOException if the JSON file can't be created.
@@ -163,25 +170,25 @@ public abstract class JsonConvert {
 	public static void apartmentsToJson(List<Apartment> listApartments) throws IOException {
 		apartmentsToJson(listApartments, APARTMENT_PATH_JSON);
 	}
-	
+
 	/**
 	 * Converts a list of Apartments to a JSON file.
 	 *
-	 * @param an <i>ArrayList</i> to convert into JSON
+	 * @param an       <i>ArrayList</i> to convert into JSON
 	 * @param jsonPath <i>String</i> the path where to create the JSON file
 	 * @throws IOException if the JSON file can't be created.
 	 */
 	public static void apartmentsToJson(List<Apartment> listApartments, String jsonPath) throws IOException {
-		
+
 		File jsonFile = new File(jsonPath);
 		Jsonb jsonb = JsonbBuilder.create();
-		
+
 		try (Writer jsonFilePath = new BufferedWriter(new FileWriter(jsonFile))) {
 			jsonFilePath.write(jsonb.toJson(listApartments));
 			jsonFilePath.close();
-			
+
 			LOGGER.info("Apartment have been converted with success");
 		}
 	}
-	
+
 }
--- src/test/java/io/github/oliviercailloux/y2018/apartments/utils/JsonConvertTests.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/utils/JsonConvertTests.java
@@ -1,4 +1,5 @@
 package io.github.oliviercailloux.y2018.apartments.utils;
+
 import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
 
 import org.junit.jupiter.api.Test;
@@ -12,113 +13,110 @@ import java.util.List;
 
 /**
  * Test class for JsonConvert
+ * 
  * @author Etienne CARTIER & Morgane FIOT
  */
 public class JsonConvertTests {
-	
+
 	/**
-	 * Tests apartmentToJson function.
-	 * Verifies if the JSON file created by the function corresponds to the expected file.
+	 * Tests apartmentToJson function. Verifies if the JSON file created by the
+	 * function corresponds to the expected file.
 	 * 
 	 * @throws IOException if the JSON file can't be created.
 	 */
 	@Test
 	void apartmentToJsonTest() throws IOException {
-		
+
 		Apartment a = new Apartment(1182118.48, "118 rue du père noel 77480", "Grand Igloo");
 		String expectedApartment = "{\"address\":\"118 rue du père noel 77480\",\"description\":\"\",\"floorArea\":1182118.48,\"floorAreaTerrace\":0.0,\"nbBathrooms\":0,\"nbBedrooms\":0,\"nbMinNight\":0,\"nbSleeping\":0,\"pricePerNight\":0.0,\"tele\":false,\"terrace\":false,\"title\":\"Grand Igloo\",\"wifi\":false}";
 		String jsonPath = "src/test/resources/io/github/oliviercailloux/y2018/apartments/readapartments/jsonfileTest.json";
-		
+
 		JsonConvert.apartmentToJson(a, jsonPath);
-		
+
 		assertEquals(expectedApartment, JsonConvert.readApartmentFromJson(jsonPath));
-		
-		assertThrows(IOException.class, () ->
-			JsonConvert.apartmentToJson(a, "")
-		);
+
+		assertThrows(IOException.class, () -> JsonConvert.apartmentToJson(a, ""));
 	}
-	
-	
+
 	/**
-	 * Tests getAddressFromJson function.
-	 * Verifies if the address extracted by the function corresponds to the expected address.
+	 * Tests getAddressFromJson function. Verifies if the address extracted by the
+	 * function corresponds to the expected address.
 	 *
 	 * @throws FileNotFoundException if the file doesn't exists.
-	 * @throws IOException if the file can't be convert into JSON format.
+	 * @throws IOException           if the file can't be convert into JSON format.
 	 */
 	@Test
 	void getAddressFromJsonTest() throws FileNotFoundException, IOException {
-		
+
 		String adressJson = "{\"data\":{\"latitude\":48.91777636365895,\"longitude\":2.4954686289120067,\"formattedAddress\":\"Allée de Turenne, Nonneville, Les Pavillons-sous-Bois, Le Raincy, Seine-Saint-Denis, Île-de-France, France métropolitaine, 93320, France\",\"country\":\"France\",\"city\":\"Les Pavillons-sous-Bois\",\"state\":\"Île-de-France\",\"zipcode\":\"93320\",\"streetName\":\"Allée de Turenne\",\"countryCode\":\"FR\",\"neighbourhood\":\"\",\"provider\":\"openstreetmap\"},\"address\":\"1 Allée de Turenne, 93320 Les Pavillons-sous-Bois\"}";
-		
+
 		assertEquals("1 Allée de Turenne, 93320 Les Pavillons-sous-Bois", JsonConvert.getAddressFromJson(adressJson));
-		
+
 	}
-	
+
 	/**
-	 * Tests apartmentsToJson function.
-	 * Verifies if the JSON file created by the function corresponds to the expected file.
+	 * Tests apartmentsToJson function. Verifies if the JSON file created by the
+	 * function corresponds to the expected file.
 	 *
 	 * @throws IOException if the JSON file can't be created.
 	 */
 	@Test
 	void apartmentsToJsonTest() throws IOException {
-		
+
 		ArrayList<Apartment> apartments = new ArrayList<>();
 		apartments.add(new Apartment(1182118.48, "118 rue du père noel 77480", "Grand Igloo"));
 		apartments.add(new Apartment(1234567.89, "123 rue du soleil", "Maison Test"));
-		
+
 		String expectedApartment = "[{\"address\":\"118 rue du père noel 77480\",\"description\":\"\",\"floorArea\":1182118.48,\"floorAreaTerrace\":0.0,\"nbBathrooms\":0,\"nbBedrooms\":0,\"nbMinNight\":0,\"nbSleeping\":0,\"pricePerNight\":0.0,\"tele\":false,\"terrace\":false,\"title\":\"Grand Igloo\",\"wifi\":false},{\"address\":\"123 rue du soleil\",\"description\":\"\",\"floorArea\":1234567.89,\"floorAreaTerrace\":0.0,\"nbBathrooms\":0,\"nbBedrooms\":0,\"nbMinNight\":0,\"nbSleeping\":0,\"pricePerNight\":0.0,\"tele\":false,\"terrace\":false,\"title\":\"Maison Test\",\"wifi\":false}]";
 		String jsonPath = "src/test/resources/io/github/oliviercailloux/y2018/apartments/readapartments/jsonListTest.json";
-		
+
 		JsonConvert.apartmentsToJson(apartments, jsonPath);
-		
+
 		assertEquals(expectedApartment, JsonConvert.readApartmentFromJson(jsonPath));
-		
-		assertThrows(IOException.class, () ->
-			JsonConvert.apartmentsToJson(apartments, "")
-		);
+
+		assertThrows(IOException.class, () -> JsonConvert.apartmentsToJson(apartments, ""));
 	}
-	
+
 	/**
-	 * Tests jsonToApartment function.
-	 * Verifies if the Apartment created by the function corresponds to the expected Apartment.
+	 * Tests jsonToApartment function. Verifies if the Apartment created by the
+	 * function corresponds to the expected Apartment.
 	 *
 	 * @throws FileNotFoundException if the file doesn't exists.
-	 * @throws IOException if the file can't be convert into JSON format.
+	 * @throws IOException           if the file can't be convert into JSON format.
 	 */
 	@Test
 	void jsonToApartmentTest() throws FileNotFoundException, IOException {
-		
+
 		Apartment apartmentRef = new Apartment(1182118.48, "118 rue du père noel 77480", "Grand Igloo");
-		
+
 		String jsonPath = "src/test/resources/io/github/oliviercailloux/y2018/apartments/readapartments/jsonfileTest.json";
 		String jsonToConvert = JsonConvert.readApartmentFromJson(jsonPath);
-		
+
 		Apartment apartmentTest = JsonConvert.jsonToApartment(jsonToConvert);
-		
+
 		assertEquals(apartmentRef.hashCode(), apartmentTest.hashCode());
 	}
-	
+
 	/**
-	 * Tests jsonToApartments function.
-	 * Verifies if the <i>ArrayList</i> of Apartment created by the function corresponds to the expected <i>ArrayList</i>.
+	 * Tests jsonToApartments function. Verifies if the <i>ArrayList</i> of
+	 * Apartment created by the function corresponds to the expected
+	 * <i>ArrayList</i>.
 	 *
 	 * @throws FileNotFoundException if the file doesn't exists.
-	 * @throws IOException if the file can't be convert into JSON format.
+	 * @throws IOException           if the file can't be convert into JSON format.
 	 */
 	@Test
 	void jsonToApartmentsTest() throws FileNotFoundException, IOException {
-		
+
 		List<Apartment> apartmentsRef = new ArrayList<>();
 		apartmentsRef.add(new Apartment(1182118.48, "118 rue du père noel 77480", "Grand Igloo"));
 		apartmentsRef.add(new Apartment(1234567.89, "123 rue du soleil", "Maison Test"));
-		
+
 		String jsonPath = "src/test/resources/io/github/oliviercailloux/y2018/apartments/readapartments/jsonListTest.json";
 		String jsonToConvert = JsonConvert.readApartmentFromJson(jsonPath);
-		
+
 		List<Apartment> apartmentsTest = JsonConvert.jsonToApartments(jsonToConvert);
-		
+
 		assertEquals(apartmentsRef.get(0).hashCode(), apartmentsTest.get(0).hashCode());
 		assertEquals(apartmentsRef.get(1).hashCode(), apartmentsTest.get(1).hashCode());
 	}
