9b4b11277daa314bd635e0e76bf9b02401463d63
av1m <36456709+av1m@users.noreply.github.com>
2020-05-01 09:45:51 +0200

    Fix #8 update junit & format file

--- src/test/java/io/github/oliviercailloux/y2018/apartments/apartment/ApartmentFactoryTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/apartment/ApartmentFactoryTest.java
@@ -1,10 +1,11 @@
 package io.github.oliviercailloux.y2018.apartments.apartment;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import io.github.oliviercailloux.y2018.apartments.apartment.Apartment.Builder;
@@ -16,39 +17,29 @@ import io.github.oliviercailloux.y2018.apartments.apartment.Apartment.Builder;
 class ApartmentFactoryTest {
 
 	/**
-	 * Test GenerateApartment function 
-	 * tests the correspondence between the results sent and those recorded.
+	 * Test GenerateApartment function tests the correspondence between the results
+	 * sent and those recorded.
 	 */
 	@Test
 	void testGenerateApartmentFromParameters() {
-		double floorArea = 456.56; 
+		double floorArea = 456.56;
 		String address = "A Random Address";
 		int nbBedrooms = 5;
 		int nbSleeping = 9;
 		int nbBathrooms = 3;
 		boolean hasTerrace = true;
-		double floorAreaTerrace = 25.32; 
-		String description = "A Random Description for A Random House in A Random Address"; 
-		String title = "A Random House"; 
+		double floorAreaTerrace = 25.32;
+		String description = "A Random Description for A Random House in A Random Address";
+		String title = "A Random House";
 		boolean wifi = true;
 		double pricePerNight = 45.95;
-		int nbMinNight = 60; 
+		int nbMinNight = 60;
 		boolean tele = false;
 		Builder apartBuilder = new Builder();
-		Apartment apart = apartBuilder.setFloorArea(floorArea)
-				.setAddress(address)
-				.setNbBedrooms(nbBedrooms)
-				.setNbSleeping(nbSleeping)
-				.setNbBathrooms(nbBathrooms)
-				.setTerrace(hasTerrace)
-				.setFloorAreaTerrace(floorAreaTerrace)
-				.setDescription(description)
-				.setTitle(title)
-				.setWifi(wifi)
-				.setPricePerNight(pricePerNight)
-				.setNbMinNight(nbMinNight)
-				.setTele(tele)
-				.build();
+		Apartment apart = apartBuilder.setFloorArea(floorArea).setAddress(address).setNbBedrooms(nbBedrooms)
+				.setNbSleeping(nbSleeping).setNbBathrooms(nbBathrooms).setTerrace(hasTerrace)
+				.setFloorAreaTerrace(floorAreaTerrace).setDescription(description).setTitle(title).setWifi(wifi)
+				.setPricePerNight(pricePerNight).setNbMinNight(nbMinNight).setTele(tele).build();
 
 		assertEquals(floorArea, apart.getFloorArea());
 		assertEquals(address, apart.getAddress());
@@ -66,13 +57,12 @@ class ApartmentFactoryTest {
 	}
 
 	/**
-	 * This function allow us to test if we have an exception thrown when the json path given
-	 * in argument of the method is wrong.
+	 * This function allow us to test if we have an exception thrown when the json
+	 * path given in argument of the method is wrong.
 	 */
 	@Test
 	public void generateApartmentFromJsonExceptionTest() {
-		assertThrows(IOException.class,
-				()-> ApartmentFactory.generateApartmentFromJsonPath(Path.of("abc")));
+		assertThrows(IOException.class, () -> ApartmentFactory.generateApartmentsFromJsonPath(Path.of("abc")));
 	}
 
 }
