a1c8dfabc07b5c668509a1c8a0d2718b9380f0ba
av1m <36456709+av1m@users.noreply.github.com>
2020-06-07 11:27:08 +0200

    add QuestionPriceArea (!build)

---
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/QuestionPriceArea.java
@@ -0,0 +1,37 @@
+package io.github.oliviercailloux.y2018.apartments.valuefunction.profile;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class QuestionPriceArea {
+  int price;
+  int surface;
+  private final String question = "Would you pay %d€ more for %d m2 more?";
+
+  private QuestionPriceArea(int price, int surface) {
+    checkNotNull(price);
+    checkNotNull(surface);
+    this.price = price;
+    this.surface = surface;
+  }
+
+  public static QuestionPriceArea create(int price, int surface) {
+    return new QuestionPriceArea(price, surface);
+  }
+
+  public int getPrice() {
+    return this.price;
+  }
+
+  public int getSurface() {
+    return this.surface;
+  }
+
+  public String getQuestion() {
+    return String.format(this.question, this.getPrice(), this.getSurface());
+  }
+
+  public void resolve(Profile p, boolean response) {
+    // RETURN LAVF
+    // p.adaptWeightRange(new range)
+  }
+}
