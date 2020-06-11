4d1da1c00490c5e30f741797f552d98baf2fa17d
EtienneCartier <et.cartier@gmail.com>
2020-06-11 11:21:40 +0200

    Changing the order of the arguments of Range.range

--- src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/ProfileTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/ProfileTest.java
@@ -20,7 +20,7 @@ public class ProfileTest {
 		  Profile.Builder profileBuilder = new Profile.Builder();
 		  
 		  for (Criterion c : Criterion.values()) {
-		      profileBuilder.setWeightRange(c, Range.range(Double.valueOf(10d), BoundType.CLOSED, Double.valueOf(0d), BoundType.CLOSED));
+		      profileBuilder.setWeightRange(c, Range.range(Double.valueOf(0d), BoundType.CLOSED, Double.valueOf(10d), BoundType.CLOSED));
 		    }	
 		  
 		  profile = profileBuilder.build();
@@ -29,8 +29,8 @@ public class ProfileTest {
 	  /** Function to test the basic Profile implementation */
 	  @Test
 	  void profileTest() {
-		  assertEquals(Double.valueOf(5d), profile.getLinearAVF().getWeight(Criterion.FLOOR_AREA), 0.0001);
-		  assertEquals(Range.range(Double.valueOf(10d), BoundType.CLOSED, Double.valueOf(0d), BoundType.CLOSED), profile.getWeightRange(Criterion.NB_BEDROOMS));
+		  assertEquals(5d, profile.getLinearAVF().getWeight(Criterion.FLOOR_AREA), 0.0001);
+		  assertEquals(Range.range(Double.valueOf(0d), BoundType.CLOSED, Double.valueOf(10d), BoundType.CLOSED), profile.getWeightRange(Criterion.NB_BEDROOMS));
 	  }
 	  
 	  /** Function to test the Profile builder */
