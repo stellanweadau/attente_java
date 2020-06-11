8c507a801f2c4dad8b7ec4fa5d1545798e906576
clemencecousin <clemence.cousin@dauphine.eu>
2020-06-11 14:54:13 +0200

    Creating the resolve function

--- src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/Profile.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/Profile.java
@@ -1,9 +1,55 @@
 package io.github.oliviercailloux.y2018.apartments.valuefunction.profile;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.google.common.collect.Range;
 import io.github.oliviercailloux.y2018.apartments.valuefunction.Criterion;
 
 public class Profile {
+
+  private Range<Double> floorAreaWeightRange;
+  private Range<Double> nbBedroomsWeightRange;
+  private Range<Double> nbSleepingWeightRange;
+  private Range<Double> nbBathroomsWeightRange;
+  private Range<Double> terraceWeightRange;
+  private Range<Double> floorAreaTerraceWeightRange;
+  private Range<Double> wifiSubjectiveValueWeightRange;
+  private Range<Double> pricePerNightSubjectiveValueWeightRange;
+  private Range<Double> nbMinNightSubjectiveValueWeightRange;
+  private Range<Double> teleSubjectiveValueWeightRange;
+
+  public Range<Double> getWeightRange(Criterion crit) {
+    switch (crit) {
+      case TELE:
+        return teleSubjectiveValueWeightRange;
+      case TERRACE:
+        return terraceWeightRange;
+      case WIFI:
+        return wifiSubjectiveValueWeightRange;
+      case FLOOR_AREA:
+        return floorAreaWeightRange;
+      case FLOOR_AREA_TERRACE:
+        return floorAreaTerraceWeightRange;
+      case NB_BATHROOMS:
+        return nbBathroomsWeightRange;
+      case NB_BEDROOMS:
+        return nbBedroomsWeightRange;
+      case NB_SLEEPING:
+        return nbSleepingWeightRange;
+      case NB_MIN_NIGHT:
+        return nbMinNightSubjectiveValueWeightRange;
+      case PRICE_PER_NIGHT:
+        return pricePerNightSubjectiveValueWeightRange;
+      default:
+        throw new IllegalArgumentException();
+    }
+  }
+
+  static double getMiddleOfRange(Range<Double> range) {
+    checkNotNull(range);
+    return (range.upperEndpoint() + range.lowerEndpoint()) / 2;
+  }
+
   public Profile setWeightRange(Criterion crit, Range<Double> value) {
     return new Profile();
   }
--- src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/QuestionPriceArea.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/QuestionPriceArea.java
@@ -2,6 +2,9 @@ package io.github.oliviercailloux.y2018.apartments.valuefunction.profile;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.collect.Range;
+import io.github.oliviercailloux.y2018.apartments.valuefunction.Criterion;
+
 public class QuestionPriceArea {
   int price;
   int surface;
@@ -31,6 +34,51 @@ public class QuestionPriceArea {
   }
 
   public void resolve(Profile p, boolean response) {
-    // p.setWeightRange()
+    Range<Double> floorAreaWeight = p.getWeightRange(Criterion.FLOOR_AREA);
+    Range<Double> priceWeight = p.getWeightRange(Criterion.PRICE_PER_NIGHT);
+
+    if (response) {
+
+      double min =
+          floorAreaWeight.lowerEndpoint() + Profile.getMiddleOfRange(floorAreaWeight) * 0.2;
+      double max = priceWeight.upperEndpoint() - Profile.getMiddleOfRange(priceWeight) * 0.1;
+
+      if (min >= floorAreaWeight.upperEndpoint()) {
+        Range<Double> r =
+            Range.closed(floorAreaWeight.upperEndpoint(), floorAreaWeight.upperEndpoint());
+        p.setWeightRange(Criterion.FLOOR_AREA, r);
+      } else {
+        Range<Double> r = Range.closed(min, floorAreaWeight.upperEndpoint());
+        p.setWeightRange(Criterion.FLOOR_AREA, r);
+      }
+      if (max <= priceWeight.lowerEndpoint()) {
+        Range<Double> r = Range.closed(priceWeight.lowerEndpoint(), priceWeight.lowerEndpoint());
+        p.setWeightRange(Criterion.PRICE_PER_NIGHT, r);
+      } else {
+        Range<Double> r = Range.closed(priceWeight.lowerEndpoint(), max);
+        p.setWeightRange(Criterion.PRICE_PER_NIGHT, r);
+      }
+    } else {
+
+      double min = priceWeight.lowerEndpoint() + Profile.getMiddleOfRange(priceWeight) * 0.2;
+      double max =
+          floorAreaWeight.upperEndpoint() - Profile.getMiddleOfRange(floorAreaWeight) * 0.1;
+
+      if (min >= priceWeight.upperEndpoint()) {
+        Range<Double> r = Range.closed(priceWeight.upperEndpoint(), priceWeight.upperEndpoint());
+        p.setWeightRange(Criterion.PRICE_PER_NIGHT, r);
+      } else {
+        Range<Double> r = Range.closed(min, priceWeight.upperEndpoint());
+        p.setWeightRange(Criterion.PRICE_PER_NIGHT, r);
+      }
+      if (max <= floorAreaWeight.lowerEndpoint()) {
+        Range<Double> r =
+            Range.closed(floorAreaWeight.lowerEndpoint(), floorAreaWeight.lowerEndpoint());
+        p.setWeightRange(Criterion.FLOOR_AREA, r);
+      } else {
+        Range<Double> r = Range.closed(floorAreaWeight.lowerEndpoint(), max);
+        p.setWeightRange(Criterion.FLOOR_AREA, r);
+      }
+    }
   }
 }
