0aea346eb1a897203a9d6f398371f51e5615d1da
av1m <36456709+av1m@users.noreply.github.com>
2020-06-18 23:16:43 +0200

    add value function class in criterion

--- src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/Criterion.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/Criterion.java
@@ -1,14 +1,29 @@
 package io.github.oliviercailloux.y2018.apartments.valuefunction;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public enum Criterion {
-  TELE,
-  TERRACE,
-  WIFI,
-  FLOOR_AREA,
-  FLOOR_AREA_TERRACE,
-  NB_BATHROOMS,
-  NB_BEDROOMS,
-  NB_SLEEPING,
-  NB_MIN_NIGHT,
-  PRICE_PER_NIGHT;
+  TELE(BooleanValueFunction.class),
+  TERRACE(BooleanValueFunction.class),
+  WIFI(BooleanValueFunction.class),
+  FLOOR_AREA(LinearValueFunction.class),
+  FLOOR_AREA_TERRACE(LinearValueFunction.class),
+  NB_BATHROOMS(LinearValueFunction.class),
+  NB_BEDROOMS(LinearValueFunction.class),
+  NB_SLEEPING(LinearValueFunction.class),
+  NB_MIN_NIGHT(ReversedLinearValueFunction.class),
+  PRICE_PER_NIGHT(ReversedLinearValueFunction.class);
+
+  private Class valueFunctionClass;
+
+  private Criterion(Class c) {
+    checkArgument(
+        PartialValueFunction.class.isAssignableFrom(c),
+        "The class must implement PartialValueFunction");
+    this.valueFunctionClass = c;
+  }
+
+  private Class getValueFunctionClass() {
+    return this.valueFunctionClass;
+  }
 }
