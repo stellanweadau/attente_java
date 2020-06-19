8d2eeceb5f2a7056e079b3182f26274fd92c074c
EtienneCartier <et.cartier@gmail.com>
2020-06-19 12:16:35 +0200

    Shapped ProfileManagerTest

--- src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/ProfileManagerTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/ProfileManagerTest.java
@@ -3,12 +3,12 @@ package io.github.oliviercailloux.y2018.apartments.valuefunction.profile;
 import org.junit.jupiter.api.Test;
 
 public class ProfileManagerTest {
-	
-	ProfileManager profileManager;
-	
-	/** Function to test the ProfileManager implementation */
-	  @Test
-	  void testProfileManager() {
-	    profileManager = ProfileManager.getInstance();
-	  }
+
+  ProfileManager profileManager;
+
+  /** Function to test the ProfileManager implementation */
+  @Test
+  void testProfileManager() {
+    profileManager = ProfileManager.getInstance();
+  }
 }
